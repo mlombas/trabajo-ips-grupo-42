d72d9bb142b1b97747b3cc146e92d12142e720c3
mocoma <mocomaminecraft@gmail.com>
2021-10-09 12:24:56 +0200

    Added pay with card

--- src/main/java/giis/bank_mockup/Bank.java
+++ src/main/java/giis/bank_mockup/Bank.java
@@ -1,6 +1,7 @@
 package giis.bank_mockup;
 
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Random;
@@ -14,6 +15,24 @@ public class Bank {
 
 	public Bank() {
 		this.pending = new HashMap<String, Integer>();
+	}
+
+	/**
+	 * Tries to pay with a credit card
+	 *
+	 * @param number the credit card number
+	 * @param expiration a date, representing the expiration date
+	 * @param ccv string with the ccv of the card
+	 *
+	 * @return true if all the data is correct
+	 */
+	public boolean payWithCard(String number, Date expiration, String ccv) {
+		if(!expiration.after(new Date())) return false;
+		if(ccv.length() != 3) return false;
+		
+		// Aqui iria el chekear que el numero esta bien
+		
+		return true;
 	}
 
 	/**
