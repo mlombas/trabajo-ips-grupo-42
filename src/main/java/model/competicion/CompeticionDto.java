cabdf40be2eb5c4968558b390c1097ebc22eca7b
mocoma <mocomaminecraft@gmail.com>
2021-10-10 13:39:40 +0200

    Added fields to CompeticionDto

--- src/main/java/model/competicion/CompeticionDto.java
+++ src/main/java/model/competicion/CompeticionDto.java
@@ -1,5 +1,14 @@
 package model.competicion;
 
-public class CompeticionDto {
+import java.sql.Date;
 
+public class CompeticionDto {
+	public String nombre;
+	public Date fecha;
+	public String tipo;
+	public double distancia;
+	public double cuota;
+	public Date fechaFin;
+	public int plazas;
+	public String estado;
 }
