5a9b0427e3eaf3ec5408a8990b3a1c0a12f6b8dc
uo277553 <uo277553@uniovi.es>
2021-10-11 20:41:31 +0200

    Comienzo de la historia de usuario para poder pagar con tarjeta

--- src/main/java/controller/atleta/AtletaCrudService.java
+++ src/main/java/controller/atleta/AtletaCrudService.java
@@ -1,5 +1,7 @@
 package controller.atleta;
 
+import java.util.Date;
+
 import model.atleta.AtletaDto;
 import model.competicion.CompeticionDto;
 
@@ -12,5 +14,15 @@ public interface AtletaCrudService {
 	 * @return datos del atleta para crear el justificante.
 	 */
 	AtletaDto registerAtletaToCompeticion(AtletaDto atleta, CompeticionDto competicion);
+	
+	
+	/**
+	 * Permite al atleta pagar con tarjeta la cuota de una competicion para poder participar en ella.
+	 * @param atleta -->  atleta que quiere inscribirse.
+	 * @param competicion --> competicion en la que quiere participar.
+	 * @return la fecha de la relización del pago
+	 */
+	Date payWithTarjeta(AtletaDto atleta, CompeticionDto competicion);
+	//TODO
 	
 }
