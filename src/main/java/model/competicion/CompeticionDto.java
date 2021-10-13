5d334afecd191dd0a2ed385841c49775a818e68d
nicolas <supernik2000@gmail.com>
2021-10-13 17:07:31 +0200

    Fixed a merge conflict

--- src/main/java/model/competicion/CompeticionDto.java
+++ src/main/java/model/competicion/CompeticionDto.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package model.competicion;
 
 import java.sql.Date;
@@ -17,21 +16,3 @@ public class CompeticionDto {
 	public int plazas;
 	public String estadoCarrera;
 }
-=======
-package model.competicion;
-
-import java.sql.Date;
-
-public class CompeticionDto {
-	public String id;
-	public String nombreCarrera;
-	public String tipoCarrera;
-	public String distancia;
-	public double cuota;
-	public Date fecha;
-	public Date fechaInicio;
-	public Date fechaFin;
-	public int plazas;
-	public String estadoCarrera;
-}
->>>>>>> main
