95534f7dec2e86c7ce3fcc6c96959dd1b0db7577
mocoma <mocomaminecraft@gmail.com>
2021-10-16 17:39:10 +0200

    Added dto getters and setters

--- src/main/java/model/competicion/CompeticionDto.java
+++ src/main/java/model/competicion/CompeticionDto.java
@@ -4,6 +4,66 @@ import java.sql.Date;
 
 public class CompeticionDto {
 	public String id;
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getNombreCarrera() {
+		return nombreCarrera;
+	}
+	public void setNombreCarrera(String nombreCarrera) {
+		this.nombreCarrera = nombreCarrera;
+	}
+	public String getTipoCarrera() {
+		return tipoCarrera;
+	}
+	public void setTipoCarrera(String tipoCarrera) {
+		this.tipoCarrera = tipoCarrera;
+	}
+	public String getDistancia() {
+		return distancia;
+	}
+	public void setDistancia(String distancia) {
+		this.distancia = distancia;
+	}
+	public double getCuota() {
+		return cuota;
+	}
+	public void setCuota(double cuota) {
+		this.cuota = cuota;
+	}
+	public Date getFecha() {
+		return fecha;
+	}
+	public void setFecha(Date fecha) {
+		this.fecha = fecha;
+	}
+	public Date getFechaInicio() {
+		return fechaInicio;
+	}
+	public void setFechaInicio(Date fechaInicio) {
+		this.fechaInicio = fechaInicio;
+	}
+	public Date getFechaFin() {
+		return fechaFin;
+	}
+	public void setFechaFin(Date fechaFin) {
+		this.fechaFin = fechaFin;
+	}
+	public int getPlazas() {
+		return plazas;
+	}
+	public void setPlazas(int plazas) {
+		this.plazas = plazas;
+	}
+	public String getEstadoCarrera() {
+		return estadoCarrera;
+	}
+	public void setEstadoCarrera(String estadoCarrera) {
+		this.estadoCarrera = estadoCarrera;
+	}
 	public String nombreCarrera;
 	public String tipoCarrera;
 	public String distancia;
