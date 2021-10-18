aa619c2f989787d0104813997f4b5ffd4d3f6e05
nicolas <supernik2000@gmail.com>
2021-10-18 19:25:55 +0200

    Añadido transformacion de posicion a tabla

---
+++ src/main/java/view/util/ClasificacionesToTable.java
@@ -0,0 +1,35 @@
+package view.util;
+
+import java.util.List;
+
+import javax.swing.JTable;
+import javax.swing.table.DefaultTableModel;
+
+import model.competicion.PosicionDto;
+
+public class ClasificacionesToTable extends JTable {
+
+	private static final long serialVersionUID = 1L;
+	
+	public ClasificacionesToTable(List<PosicionDto> posiciones) {
+		setDefaultEditor(Object.class, null); // hacer que no sea editable
+		
+		String[] columnNames = {"DNI", "nombre", "categoria", "fechaInscripcion", "estadoInscripcion"};
+		DefaultTableModel model = new DefaultTableModel(null, columnNames);
+		
+		for(PosicionDto posicion : posiciones) {
+			Object[] fila = new Object[columnNames.length];
+			fila[0] = posicion.posicion;
+			fila[1] = posicion.sexo;
+			fila[2] = posicion.nombreAtleta;
+			if (posicion.estadoInscripcion == "Finalizado")
+				fila[3] = posicion.tiempo + "";
+			else
+				fila[3] ="-:--";			
+			model.addRow(fila);
+		}
+		
+		this.setModel(model);
+	}
+
+}
