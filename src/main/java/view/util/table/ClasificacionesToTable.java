13f09c0c28d7d73300bd33151a7beda6f166b126
nicolas <supernik2000@gmail.com>
2021-11-05 20:13:30 +0100

    Añadidia parte de la tabla de competiciones

--- src/main/java/view/util/table/ClasificacionesToTable.java
+++ src/main/java/view/util/table/ClasificacionesToTable.java
@@ -14,15 +14,19 @@ public class ClasificacionesToTable extends JTable {
 	public ClasificacionesToTable(List<ClasificacionDto> posiciones) {
 		setDefaultEditor(Object.class, null); // hacer que no sea editable
 		
-		String[] columnNames = {"posicion", "sexo", "nombre", "tiempo"};
+		String[] columnNames = {"Posición", "Dorsal", "Nombre", "Categoría", "Sexo", "Tiempo", "Salida", "Llegada"};
 		DefaultTableModel model = new DefaultTableModel(null, columnNames);
 		
 		for(ClasificacionDto posicion : posiciones) {
 			Object[] fila = new Object[columnNames.length];
 			fila[0] = posicion.posicion;
-			fila[1] = posicion.sexo;
+			fila[1] = posicion.dorsal;
 			fila[2] = posicion.nombre;
-			fila[3] = posicion.tiempoLlegada-posicion.tiempoSalida;			
+			fila[3] = posicion.categoria;
+			fila[4] = posicion.sexo;
+			fila[5] = posicion.formatTime(posicion.tiempoLlegada-posicion.tiempoSalida);
+			fila[6] = posicion.formatTime(posicion.tiempoSalida);
+			fila[7] = posicion.formatTime(posicion.tiempoLlegada);
 			model.addRow(fila);
 		}
 		
