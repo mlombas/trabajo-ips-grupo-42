7379dced5a419b070c6c2fd09a36ecf36cf95473
mocoma <mocomaminecraft@gmail.com>
2021-11-05 18:18:26 +0100

    Created csv reading system

---
+++ src/main/java/util/csv/CSVCreator.java
@@ -0,0 +1,21 @@
+package util.csv;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+
+public class CSVCreator {
+	public CSVTable<String, String> read(String fname) throws FileNotFoundException {
+		return read(fname, "\s*,\s*");
+	}
+	
+	public CSVTable<String, String> read(String fname, String split) throws FileNotFoundException {
+		File f = new File(fname);
+		CSVReader reader = new CSVReader(f, split);
+		
+		CSVTable<String, String> table = reader.read();
+		
+		reader.close();
+		
+		return table;
+	}
+}
