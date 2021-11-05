716ab5f863ed135c935ed7390b55e62250491fdc
nicolas <supernik2000@gmail.com>
2021-11-05 19:14:21 +0100

    Historia 15157 hecha

--- src/main/java/model/competicion/ClasificacionDto.java
+++ src/main/java/model/competicion/ClasificacionDto.java
@@ -4,16 +4,42 @@ package model.competicion;
 public class ClasificacionDto {
 
 	public int posicion;
+	public int dorsal;
+	public int tiempoSalida;
+	public int tiempoLlegada;
 	public String sexo;
-	public String nombreAtleta;
-	public double tiempo;
-	public String estadoInscripcion; //TODO Nico cambiar esto por el enum
+	public String nombre;
+	public String categoria;
 	
+	public String getCategoria() {
+		return categoria;
+	}
+	public void setCategoria(String categoria) {
+		this.categoria = categoria;
+	}
 	public int getPosicion() {
 		return posicion;
 	}
 	public void setPosicion(int posicion) {
 		this.posicion = posicion;
+	}
+	public int getDorsal() {
+		return dorsal;
+	}
+	public void setDorsal(int dorsal) {
+		this.dorsal = dorsal;
+	}
+	public int getTiempoSalida() {
+		return tiempoSalida;
+	}
+	public void setTiempoSalida(int tiempoSalida) {
+		this.tiempoSalida = tiempoSalida;
+	}
+	public int getTiempoLlegada() {
+		return tiempoLlegada;
+	}
+	public void setTiempoLlegada(int tiempoLllegada) {
+		this.tiempoLlegada = tiempoLllegada;
 	}
 	public String getSexo() {
 		return sexo;
@@ -21,24 +47,11 @@ public class ClasificacionDto {
 	public void setSexo(String sexo) {
 		this.sexo = sexo;
 	}
-	public String getNombreAtleta() {
-		return nombreAtleta;
+	public String getNombre() {
+		return nombre;
 	}
-	public void setNombreAtleta(String nombreAtleta) {
-		this.nombreAtleta = nombreAtleta;
+	public void setNombre(String nombreAtleta) {
+		this.nombre = nombreAtleta;
 	}
-	public double getTiempo() {
-		return tiempo;
-	}
-	public void setTiempo(double tiempo) {
-		this.tiempo = tiempo;
-	}
-	public String getEstadoInscripcion() {
-		return estadoInscripcion;
-	}
-	public void setEstadoInscripcion(String estadoInscripcion) {
-		this.estadoInscripcion = estadoInscripcion;
-	}
-	
-	
+
 }
