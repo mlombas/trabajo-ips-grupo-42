34a49d94cd7f11c44d4fd3fd46ca6931dcc7aa68
Ángel Iglesias Préstamo <65736636+angelip2303@users.noreply.github.com>
2021-11-06 16:27:42 +0100

    arreglando pago con transferencia

--- src/main/java/model/inscripcion/EstadoInscripcion.java
+++ src/main/java/model/inscripcion/EstadoInscripcion.java
@@ -2,17 +2,7 @@ package model.inscripcion;
 
 public enum EstadoInscripcion {
 
-	PRE_INSCRITO("PRE-INSCRITO");
-	
-	
-	private String estado = "";
-	
-	EstadoInscripcion(String estado) {
-		this.estado = estado;
-	}
-	
-	public String toString() {
-		return this.estado;
-	}
+	PRE_INSCRITO,
+	INSCRITO;
 	
 }
